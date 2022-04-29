2a17f75a94fc5cadb7188d0f2be4f5abc5abf2b1
Ramiz <Ramizworking@gmail.com>
2022-04-29 23:12:55 +0500

    init git

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {loader} = require("mini-css-extract-plugin");
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+console.log('IS DEV: ', isDev)
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        main: './index.js',
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        port: 5000
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+        ],
+    },
+};
